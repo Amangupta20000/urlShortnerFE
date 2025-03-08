8f91641232f71468e987b36e9829d56b0726fbe8
Aman Gupta <amangupta20000@gmail.com>
2025-03-08 16:41:58 +0530

    Backup APIs Added

--- src/ApiContants.js
+++ src/ApiContants.js
@@ -1,3 +1,11 @@
+// heroku APIs (Backup)
+// export const urlShortner = {
+//   SHORTEN_URL: `https://aman-url-shortner-008361b548de.herokuapp.com/api/shorturl`, // Endpoint for shortening URLs (POST)
+//   REDIRECT_URL: `https://aman-url-shortner-008361b548de.herokuapp.com/redirect/`, // Endpoint for getting all shortened URLs (GET)
+// };
+// https://git.heroku.com/aman-url-shortner.git
+//
+
 export const urlShortner = {
   SHORTEN_URL: `https://urlshortner-223369017943.asia-south2.run.app/api/shorturl`, // Endpoint for shortening URLs (POST)
   REDIRECT_URL: `https://urlshortner-223369017943.asia-south2.run.app/redirect/`, // Endpoint for getting all shortened URLs (GET)
